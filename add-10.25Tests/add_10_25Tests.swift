588e211d2359dce87d10a617f32a360536a015f2
Unstoppable20171103191 <1163681662@qq.com>
2018-10-26 19:57:03 +0800

    Initial Commit

---
+++ add-10.25Tests/add_10_25Tests.swift
@@ -0,0 +1,34 @@
+//
+//  add_10_25Tests.swift
+//  add-10.25Tests
+//
+//  Created by s20171103191 on 2018/10/26.
+//  Copyright © 2018年 s20171103191. All rights reserved.
+//
+
+import XCTest
+@testable import add_10_25
+
+class add_10_25Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
